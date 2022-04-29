7218279fff8feeb1fccda72357ca69d8e0905d26
ROMULO CEZAR <101668013+romulocz18@users.noreply.github.com>
2022-04-29 16:28:59 -0300

    Add files via upload

---
+++ MêsSe.C
@@ -0,0 +1,42 @@
+// Esse algoritmo mostra qual o mês correspondete, de acordo com numero interio; 
+
+
+#include <stdio.h>
+main() // metodo principal;
+{
+    int mes;
+
+    printf("\n Digite numero para para serber o mes correspondente: "); // qual o numero 
+    scanf("%d",&mes);
+
+ if (mes == 1)//condições
+ printf("\n O mes e igual a janeiro %d",mes); // saída
+else if(mes == 2)
+printf("\n O mes e igual a fevereiro %d",mes);// saída
+else if(mes == 3)
+printf("\n O mes e igual a março %d",mes);// saída 
+else if(mes == 4)
+printf("\n O mes e igual a abril %d",mes);// saída 
+else if(mes == 5)
+printf("\n O mes e igual a maio %d",mes);// saída 
+else if(mes == 6)
+printf("\n O mes e igual a junho %d",mes);// saida 
+else if(mes == 7)
+printf("\n O mes e igual a julho %d",mes);// saída 
+else if(mes == 8)
+printf("\n O mes e igual a agosto %d",mes);// saída 
+else if(mes == 9)
+printf("\n O mes e igual a setembro %d",mes);// saida 
+else if(mes == 10)
+printf("\n O mes e igual a outubro %d",mes);// saída 
+else if(mes == 11)
+printf("\n O mes e igual a novembro %d",mes);// saída
+else if(mes == 12)
+printf("\n O mes e igual a dezembro %d",mes);//saída 
+else
+printf( "\n esse numero não correspodem aos meses do ano!");//saida
+
+printf("\n\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Fim de sistema>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+}
+
